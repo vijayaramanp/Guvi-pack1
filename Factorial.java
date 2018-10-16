511f10b3161f55948b15b0f1d5c569b3f61ed3e2
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2018-10-16 21:56:49 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,1 +1,28 @@
-
+//nput size N<=20
+import java.util.Scanner;
+class Factorial{
+ public static void main(String[] qwe){
+  Scanner sc=new Scanner(System.in);
+   int N=sc.nextInt();
+   int fact=1;
+   if(N==0||N==1){
+    fact=1; 		
+	System.out.print(fact);
+   }
+   else if(N<=20){
+     while(N!=0){
+       fact=fact*N;
+       N--;
+     }
+	 System.out.print(fact);
+   }
+   else{
+	   
+	   System.out.print("limit exceeds");
+   }
+   
+   
+ }
+  
+  
+}
