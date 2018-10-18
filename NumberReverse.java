f875bb26a10c12cf6cf34b131abda0f2a0ec4f83
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2018-10-19 00:09:42 +0900

    Update NumberReverse.java

--- NumberReverse.java
+++ NumberReverse.java
@@ -1,1 +1,14 @@
 //Reverse a given numbers
+import java.util.Scanner;
+class NumberReverse{
+ public static void main(String []qwe){
+   int n=sc.nextInt();
+   int R=0,digit=0;
+   while(n!=0){
+     digit=n/10;
+     R=R*10+digit;
+     n=n/10;
+   }
+   System.out.println(R);
+ }
+}
