7ad0cec89594bbc596546a94967c800b8ce17684
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2018-10-19 00:31:47 +0900

    Create StringAppend.java

---
+++ StringAppend.java
@@ -0,0 +1,10 @@
+//Append a period at end of the string
+import java.util.Scanner;
+class StringAppend{
+ public static void main(String []qwe){
+	 Scanner sc=new Scanner(System.in);
+	String S=sc.next();
+	S=S+'.';
+   System.out.println(S);
+ }
+}
