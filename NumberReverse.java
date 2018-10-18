49aa9f642bbc90c3c0c92e6f6fa3de29ad30fd4e
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2018-10-19 00:23:46 +0900

    Update NumberReverse.java

--- NumberReverse.java
+++ NumberReverse.java
@@ -2,12 +2,13 @@
 import java.util.Scanner;
 class NumberReverse{
  public static void main(String []qwe){
-   int n=sc.nextInt();
-   int R=0,digit=0;
-   while(n!=0){
-     digit=n/10;
-     R=R*10+digit;
-     n=n/10;
+	 Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	int R=0,digit=0;
+	while(n!=0){
+		digit=n%10;
+		R=R*10+digit;
+		n=n/10;
    }
    System.out.println(R);
  }
