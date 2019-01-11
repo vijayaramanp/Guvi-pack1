b442e5b4b31ca709a61448b259d2c3603b134ffa
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2019-01-11 10:45:08 +0530

    Update ap_sum.py

--- ap_sum.py
+++ ap_sum.py
@@ -1,7 +1,9 @@
 N,A,D=raw_input().split()
-i=int(A)
+i=1
 ap_sum=0
+temp=int(A)
 while i<=int(N):
-    ap_sum=ap_sum+i
-    i=i+int(D)
+    ap_sum=ap_sum+temp
+    temp=temp+int(D)
+    i=i+1
 print(ap_sum)
